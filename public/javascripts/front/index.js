ca666d133725f1867e9dfd641e9068f023b42108
IceEnd <min@coolecho.net>
2016-07-17 01:40:29 +0800

    动态获取DISQUS评论

--- public/javascripts/front/index.js
+++ public/javascripts/front/index.js
@@ -5,6 +5,8 @@
         article_ul = $('#article-ul'),
         article_li = $('.article-li');
 
+    var PATH = location.protocol + '//' + location.hostname + '/article/av';
+
     var page = 1,h,t,s_top;
     var flag = true;
     //初始化
@@ -43,8 +45,8 @@
                         htmlStr += '<li class="article-li"><article><div class="article-info-div"><div class="article-user-img-div float-left"><img src="'+
                             blogs[b].head_img+'"></div><div class="vertical-line-div float-left"><div class="vertical-line"></div></div><div class="article-info float-left">'+
                             '<h3 class="article-title-h"><span class="shine-font-span"></span><a href="/article/av'+blogs[b].id+'">'+blogs[b].title+'</a>'+
-                            '</h3> <div class="article-other-info"><span><a class="article-author">'+blogs[b].username+'</a></span><span><a class="article-comments-num" href="https://localhost/article/av>'+
-                            blogs[b].id+'#disqus_thread">0 Comment</a></span><span><a class="article-view-num">'+blogs[b].view_num+'</a></span><span><a class="article-date">'+
+                            '</h3> <div class="article-other-info"><span><a class="article-author">'+blogs[b].username+'</a></span><span><a class="article-comments-num disqus-comment-count" data-disqus-url="'+PATH+
+                            blogs[b].id+'" href="'+PATH+blogs[b].id+'#disqus_thread">0 Comment</a></span><span><a class="article-view-num">'+blogs[b].view_num+'</a></span><span><a class="article-date">'+
                             (new Date(blogs[b].publish_date)).getFullYear()+'-'+((new Date(blogs[b].publish_date)).getMonth()+1)+'-'+(new Date(blogs[1].publish_date)).getDate()+'</a></span></div></div>'+
                             '<div class="clearfix"></div></div>';
 
@@ -73,6 +75,7 @@
                         loadMoreBtnDiv.css('display','inline-block');
                     }
                     article_li = $('.article-li');
+                    getCommentCount();
                 }
                 else{
                     myAlert('网络繁忙，请稍后再试...');
@@ -141,5 +144,33 @@
             flag = false;
         }
     });
+
+    //获取DISQUS评论个数
+    function getCommentCount() {
+        var disqusPublicKey = "RR7gTr0UICxmuw0KzRf6kx379osnkz6ePIGlcjN9B8O1q8Rv81SQkQ6JMTGtA6Hu";
+        var disqusShortname = "coolecho";
+        var urlArray = [];
+
+        $('.disqus-comment-count').each(function () {
+            var url = $(this).attr('data-disqus-url');
+            urlArray.push('link:' + url);
+        });
+
+        $.ajax({
+            type: 'GET',
+            url: "https://disqus.com/api/3.0/threads/set.jsonp",
+            data: { api_key: disqusPublicKey,forum : disqusShortname, thread : urlArray }, // URL method
+            cache: false,
+            dataType: "jsonp",
+            success: function (result) {
+                for (var i in result.response) {
+                    var countText = " comments";
+                    var count = result.response[i].posts;
+                    if (count == 1) countText = " comment";
+                        $('.disqus-comment-count[data-disqus-url="' + result.response[i].link + '"]').html(count + countText);
+                }
+            }
+        });
+    }
 
 })($);
