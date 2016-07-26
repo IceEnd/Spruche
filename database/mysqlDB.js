4e286de2dc64d652925e5bc06559b8a2878bc61d
IceEnd <min@coolecho.net>
2016-07-26 21:40:03 +0800

    隐藏密码

--- database/mysqlDB.js
+++ database/mysqlDB.js
@@ -1,8 +1,8 @@
 module.exports = {
   mysql: {
     host: '127.0.0.1', 
-    user: 'root',       //username,用户名
-    password: '2720mh',       //password,密码
+    user: 'username',       //username,用户名
+    password: '******',       //password,密码
     database:'database',    //database，数据库名
     port: 3306              //端口
   }
